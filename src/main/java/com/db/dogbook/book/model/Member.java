aa77bcdb704cceb620ec8b3236d735c47ef582ab
Fangsangik <hwangsangik@gmail.com>
2024-10-11 18:13:10 +0900

    feat = "equals & hashcode"

--- src/main/java/com/db/dogbook/book/model/Member.java
+++ src/main/java/com/db/dogbook/book/model/Member.java
@@ -4,10 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
 
 @Data
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -16,4 +24,17 @@ public class Member {
     private int age;
     private String email;
     private String phoneNumber;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return age == member.age && Objects.equals(id, member.id) && Objects.equals(userId, member.userId) && Objects.equals(password, member.password) && Objects.equals(email, member.email) && Objects.equals(phoneNumber, member.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId, password, age, email, phoneNumber);
+    }
 }
